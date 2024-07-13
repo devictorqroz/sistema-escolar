a46ba99acca3045834dfb6aee758269bc4168644
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-07-13 06:10:00 -0300

    Create aluno builder example

--- src/main/java/com/devictor/sistemaescolar/Aluno.java
+++ src/main/java/com/devictor/sistemaescolar/Aluno.java
@@ -9,18 +9,27 @@ public class Aluno {
     // JPA
     // Clean Arch
 
-    private String cpf;
+
+    private CPF cpf;
     private String nome;
 
     private Email email;
 
     private List<Telefone> telefones = new ArrayList<>();
 
+
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+
     public void adicionarTelefone(String ddd, String numero) {
         this.telefones.add(new Telefone(ddd, numero));
     }
 
-    public String getCpf() {
+    public CPF getCpf() {
         return cpf;
     }
 
---
+++ src/main/java/com/devictor/sistemaescolar/FabricaDeAluno.java
@@ -0,0 +1,35 @@
+package com.devictor.sistemaescolar;
+
+// Parecido com padrão Builder
+public class FabricaDeAluno {
+
+    private Aluno aluno;
+
+    public FabricaDeAluno comNomeCPFEEmail(String nome, String cpf, String email) {
+        this.aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+        return this;
+    }
+
+    public FabricaDeAluno comTelefone(String ddd, String numero) {
+        this.aluno.adicionarTelefone(ddd, numero);
+        return this;
+    }
+
+    public Aluno criar() {
+        return this.aluno;
+    }
+
+
+    // Melhorias
+    // Chamada única do método construtor
+    // comTelefone() não ser chamado antes do método construtor
+    public static void main(String[] args) {
+        FabricaDeAluno fabrica = new FabricaDeAluno();
+        Aluno novoAluno = fabrica.comNomeCPFEEmail("", "", "")
+                .comTelefone("", "")
+                .comTelefone("", "")
+                .criar();
+    }
+
+
+}
